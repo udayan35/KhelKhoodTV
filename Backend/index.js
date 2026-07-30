f558dfb8fdd35faee7f4f129f5f57b148b0ac899
udayan35 <udayanpande7@gmail.com>
2026-07-30 11:50:40 +0530

    Fix __dirname for ES modules

--- Backend/index.js
+++ Backend/index.js
@@ -18,7 +18,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 import { fileURLToPath } from "url";
-import path from "path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
